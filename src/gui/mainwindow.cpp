c2d9bf5a1aa6e0bcbf16229930ebcf0e7905800c
lzlaG <p0chta1234prosto@yandex.ru>
2024-11-11 22:08:01 +0500

    select file path

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -1,14 +1,32 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFile>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QCompleter>
+#include <QFileSystemModel>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->ScanDirLine->setReadOnly(true);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_SetupDirButton_clicked()
+{
+    QString ScanDir = QFileDialog::getExistingDirectory(this, tr("Open Directory"),
+                                                "",
+                                                QFileDialog::ShowDirsOnly
+                                                | QFileDialog::DontResolveSymlinks);
+    if (ScanDir.isEmpty())
+            return;
+    ui->ScanDirLine->setText(ScanDir);
+}
+
